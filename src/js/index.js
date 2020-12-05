9089375eb63dd1f99858173c8475352c3adb99cf
sathyaramkumar <70505050+sathyaramkumar@users.noreply.github.com>
2020-12-05 17:44:58 +0530

    stying navbar

---
+++ src/js/index.js
@@ -0,0 +1,10 @@
+function navToggler() {
+    var toggler = document.getElementById("navbarNav");
+
+    if (toggler.style.display === "block") {
+        toggler.style.display = "none";
+    }
+    else {
+        toggler.style.display = "block";
+    }
+};

26d57268f82e267e068799a4a28974901632ed72
sathyaramkumar <70505050+sathyaramkumar@users.noreply.github.com>
2020-12-06 18:13:17 +0530

    styling other section

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,19 @@
-function navToggler() {
-    var toggler = document.getElementById("navbarNav");
+// function navToggler() {
+//     var toggler = document.getElementById("navbarNav");
 
-    if (toggler.style.display === "block") {
-        toggler.style.display = "none";
-    }
-    else {
-        toggler.style.display = "block";
-    }
-};
+//     if (toggler.style.display === "block") {
+//         toggler.style.display = "none";
+//     }
+//     else {
+//         toggler.style.display = "block";
+//     }
+// };
+
+// function myFunction() {
+//     var x = document.getElementById("navbarNav");
+//     if (x.className === "navbar-nav") {
+//       x.className += " responsive";
+//     } else {
+//       x.className = "navbar-nav";
+//     }
+//   }
